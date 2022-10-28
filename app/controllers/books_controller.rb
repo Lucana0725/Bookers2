2a11cfaf8932549eecc60ea177cd9fc2aedbbae0
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-10-28 15:46:35 +0000

    [Finish]book_controllerのcreateアクションとストロングパラメーター、ルーティングにcreateの追加、投稿機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,13 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
 
   def index
   end
@@ -10,5 +17,12 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+  
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
