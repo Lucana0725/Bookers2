08933df8ec94620073350f467ee796e89c215e43
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-11-05 14:38:41 +0000

    [Finish]未ログイン時にはログインページへ飛ばされるよう実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+  # 未ログイン時にアクセスできるのはhomes#topのみ に制限する。
+  # :authenticate_user! ---> 未ログイン時はログインページに飛ばすというauthedeviseのメソッド。
+  before_action :authenticate_user!, except: [:top]
+  
+  # ストロングパラメーターを反映させるdeviseのメソッド。
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  
   
   def after_sign_in_path_for(resource)
     user_path(current_user.id)
@@ -8,6 +15,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
+  
+  
   
   protected
   
