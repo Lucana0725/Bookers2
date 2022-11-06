ec88cfea29f4b85c8a76dfee7315a420fd063f89
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-11-06 05:46:27 +0000

    [Finish]他人のユーザー編集画面へアクセスできないよう実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,12 @@ class UsersController < ApplicationController
   end
 
   def edit
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if (user_id != login_user_id)
+      redirect_to user_path(current_user.id)
+    end
+    
     @user = User.find(params[:id])
   end
   
